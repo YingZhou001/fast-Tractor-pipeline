328589a975d4b895cb5867df2ee9f1b3cd0764e7
YingZhou001 <31318632+YingZhou001@users.noreply.github.com>
2024-04-30 10:06:30 -0400

    add alternative formating script in python

--- 4.ancestry.extraction/run.sh
+++ 4.ancestry.extraction/run.sh
@@ -7,6 +7,19 @@ do
 
 dir=../tmp/${chr}
 
+if false
+then
+# for python formating
+## slow!!!
+job=O.${chr}.${cohorttag}.anc${anc}
+sbatch --mem 40G -J ${job} -o ${job}.%j --wrap="
+bash record_py.sh ${chr} ${cohorttag} ${dir}
+"
+fi
+
+if true
+then
+# for C formating
 for anc in 0 1
 do
 
@@ -15,6 +28,7 @@ job=O.${chr}.${cohorttag}.anc${anc}
 sbatch --mem 40G -J ${job} -o ${job}.%j --wrap="
 bash record.sh ${chr} ${anc} ${cohorttag} ${dir}
 "
+fi
 
 done
 done
